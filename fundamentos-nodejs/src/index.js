e5409f5e5479a7158345a12af4c5fb6f2033e1da
Gustavo Henrique <gustavo._henrique@hotmail.com>
2022-07-28 15:33:48 -0300

    feat: Using HTTP methods

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -2,8 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World Ignite - Fundamentos NodeJS!" });
+app.get("/courses", (request, response) => {
+  return response.json(["Node.js", "React", "React Native"]);
+});
+
+app.post("/courses", (request, response) => {
+  return response.json(["Node.js", "React", "React Native", "Elixir"]);
+});
+
+app.put("/courses/:id", (request, response) => {
+  return response.json(["NodeJS", "React", "React Native", "Elixir"]);
+});
+
+app.patch("/courses/:id", (request, response) => {
+  return response.json(["Node.js", "ReactJS", "React Native", "Elixir"]);
+});
+
+app.delete("/courses/:id", (request, response) => {
+  return response.json(["Node.js", "React", "React Native"]);
 });
 
 app.listen(3333);
