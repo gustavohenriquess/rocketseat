c06a7ed2889d4342dbc1191c0251cdf8cf1c3037
Gustavo Henrique <gustavo._henrique@hotmail.com>
2023-01-31 18:27:43 -0300

    feat: finish create template

--- ignite-2022/serveless/certificateignite/src/functions/generateCertificate.ts
+++ ignite-2022/serveless/certificateignite/src/functions/generateCertificate.ts
@@ -1,11 +1,30 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import { document } from "src/utils/dynamodbClient";
+import { compile } from "handlebars";
+import dayjs from "dayjs";
+import { readFileSync } from "fs";
+import { join } from "path";
 
 interface ICreateCertificate {
   id: string;
   name: string;
   grade: string;
 }
+
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  medal: string;
+  date: string;
+}
+
+const compileTemplate = async (data: ITemplate) => {
+  const filePath = join(process.cwd(), "src", "templates", "certificate.hbs");
+  const html = readFileSync(filePath, "utf-8");
+
+  return compile(html)(data);
+};
 
 export const handler: APIGatewayProxyHandler = async (event) => {
   const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
@@ -32,6 +51,18 @@ export const handler: APIGatewayProxyHandler = async (event) => {
     })
     .promise();
 
+  const medalPath = join(process.cwd(), "src", "templates", "selo.png");
+  const medal = readFileSync(medalPath, "base64");
+  const data: ITemplate = {
+    id,
+    name,
+    grade,
+    medal,
+    date: dayjs().format("DD/MM/YYYY"),
+  };
+
+  const content = await compileTemplate(data);
+  console.log(content);
   return {
     statusCode: 201,
     body: JSON.stringify(response.Items[0]),
