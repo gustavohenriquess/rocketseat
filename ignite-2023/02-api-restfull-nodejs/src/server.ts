d53c6e360316b0d21846c07c9f2820d49f42c6f8
Gustavo Henrique <gustavo._henrique@hotmail.com>
2023-04-04 08:26:44 -0300

    feat: added knex, dotenv and created migrations

--- ignite-2023/02-api-restfull-nodejs/src/server.ts
+++ ignite-2023/02-api-restfull-nodejs/src/server.ts
@@ -1,9 +1,28 @@
 import fastify from 'fastify'
+import { knex } from './database'
+import crypto from 'crypto'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello World!'
+app.post('/hello', async () => {
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Teste de transação',
+      amount: 1000,
+      session_id: crypto.randomUUID(),
+    })
+    .returning('*')
+
+  return transaction
+})
+
+app.get('/hello', async () => {
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transactions
 })
 
 app.listen({ port: 3333 }).then(() => console.log('Server is running...'))
