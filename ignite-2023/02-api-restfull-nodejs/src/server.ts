b227dbe5882a0f64caff0996f9785eaf006e0ada
Gustavo Henrique <gustavo._henrique@hotmail.com>
2023-04-18 21:50:42 -0300

    feat: added automated tests

--- ignite-2023/02-api-restfull-nodejs/src/server.ts
+++ ignite-2023/02-api-restfull-nodejs/src/server.ts
@@ -1,11 +1,4 @@
-import fastify from 'fastify'
-import cookie from '@fastify/cookie'
+import { app } from './app'
 import { env } from './env'
-import { transactionRoutes } from './routes/transaction'
-
-const app = fastify()
-
-app.register(cookie)
-app.register(transactionRoutes, { prefix: '/transactions' })
 
 app.listen({ port: env.PORT }).then(() => console.log('Server is running...'))
