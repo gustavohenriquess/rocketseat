e461b4a6977dc8a966801458ec43ef8ed8738e7c
Gustavo Henrique <gustavo._henrique@hotmail.com>
2024-01-30 11:37:28 -0300

    feat: created prisma gym repository

---
+++ ignite-2023/03-solid-api-nodejs/src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,37 @@
+import { Gym, Prisma } from '@prisma/client'
+import { FindManyNearbyParams, GymsRepository } from '../gyms-repository'
+import { prisma } from '@/lib/prisma'
+
+export class PrismaGymRepository implements GymsRepository {
+  async create(data: Prisma.GymCreateInput) {
+    return await prisma.gym.create({
+      data,
+    })
+  }
+
+  async searchMany(query: string, page: number) {
+    return await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+        },
+      },
+      skip: (page - 1) * 20,
+      take: 20,
+    })
+  }
+
+  async findManyNearby({ latitude, longitude }: FindManyNearbyParams) {
+    return await prisma.$queryRaw<Gym[]>`
+    SELECT * from gyms
+    WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10`
+  }
+
+  async findById(id: string) {
+    return await prisma.gym.findUnique({
+      where: {
+        id,
+      },
+    })
+  }
+}
