96913d662eb9cafad35faef83c190b13bed5a87e
Gustavo Henrique <gustavo._henrique@hotmail.com>
2024-01-31 12:11:29 -0300

    test: creating test environment

---
+++ ignite-2023/03-solid-api-nodejs/prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,14 @@
+import { Environment } from 'vitest'
+
+export default <Environment>{
+  name: 'prisma',
+  transformMode: 'ssr',
+  async setup() {
+    console.log('Setup')
+    return {
+      teardown() {
+        console.log('Teardown')
+      },
+    }
+  },
+}
---
+++ ignite-2023/03-solid-api-nodejs/src/http/controllers/register.spec.ts
@@ -0,0 +1,5 @@
+import { expect, test } from 'vitest'
+
+test('OK', () => {
+  expect(1).toBe(1)
+})
