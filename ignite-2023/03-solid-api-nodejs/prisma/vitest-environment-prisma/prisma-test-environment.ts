b71954962452c0a7079c69e00a57ac3a3b0d3a91
Gustavo Henrique <gustavo._henrique@hotmail.com>
2024-01-31 15:54:23 -0300

    test: setting database schema for tests e2e

--- ignite-2023/03-solid-api-nodejs/prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ ignite-2023/03-solid-api-nodejs/prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,39 @@
+import { PrismaClient } from '@prisma/client'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('The DATABASE_URL environment variable is missing.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   transformMode: 'ssr',
   async setup() {
     console.log('Setup')
+    const schema = randomUUID()
+    process.env.DATABASE_URL = generateDatabaseURL(schema)
+
+    execSync(`npx prisma migrate deploy`)
+
     return {
-      teardown() {
+      async teardown() {
         console.log('Teardown')
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+        await prisma.$disconnect()
       },
     }
   },
