4817a4d239f8aa9d6d9d157ed2ee03c4c12b7c80
Gustavo Henrique <gustavo._henrique@hotmail.com>
2024-08-27 15:57:05 -0300

    feat: mapping properties

--- ignite-2023/04-clean-ddd/src/domain/entities/question.ts
+++ ignite-2023/04-clean-ddd/src/domain/entities/question.ts
@@ -1,11 +1,16 @@
 import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityId } from "../../core/entities/unique-entity-id"
 
 interface QuestionProps {
+  authorId: UniqueEntityId
+  bestAnswerId?: UniqueEntityId
   title: string
-  slug: Slug
   content: string
-  authorId: string
+  slug: Slug
+  createdAt: Date
+  updatedAt?: Date
+
 }
 
 export class Question extends Entity<QuestionProps> { }
