d980cf6bf2ac21f36c0f10691efa1a81493c4461
Gustavo Henrique <gustavo._henrique@hotmail.com>
2024-08-27 17:42:06 -0300

    feat: abstracting entities and adding getters and setters

--- ignite-2023/04-clean-ddd/src/core/entities/entity.ts
+++ ignite-2023/04-clean-ddd/src/core/entities/entity.ts
@@ -3,10 +3,10 @@ import { UniqueEntityId } from "./unique-entity-id"
 
 export class Entity<Props> {
   private _id: UniqueEntityId
-  protected props: any
+  protected props: Props
 
-  protected constructor(props: Props, id?: string) {
-    this._id = new UniqueEntityId(id)
+  protected constructor(props: Props, id?: UniqueEntityId) {
+    this._id = id ?? new UniqueEntityId()
     this.props = props
   }
 
--- ignite-2023/04-clean-ddd/src/domain/entities/answer.ts
+++ ignite-2023/04-clean-ddd/src/domain/entities/answer.ts
@@ -1,5 +1,7 @@
+import dayjs from "dayjs"
 import { Entity } from "../../core/entities/entity"
 import { UniqueEntityId } from "../../core/entities/unique-entity-id"
+import { Optional } from "../../core/types/optional"
 
 interface AnswerProps {
   authorId: UniqueEntityId
@@ -12,5 +14,42 @@ interface AnswerProps {
 export class Answer extends Entity<AnswerProps> {
   get content() {
     return this.props.content
+  }
+
+  get questionId() {
+    return this.props.questionId
+  }
+
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get isNew(): boolean {
+    return dayjs().diff(this.createdAt, 'days') <= 3
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat('...')
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  static create(props: Optional<AnswerProps, 'createdAt'>, id?: UniqueEntityId) {
+    const answer = new Answer({
+      ...props,
+      createdAt: new Date()
+    }, id)
+
+    return answer
   }
 }
